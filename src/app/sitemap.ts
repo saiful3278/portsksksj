ab130c56a8d9a2f303760020e38e150b16ce1940
saiful3278G <mdm439382@gmail.com>
2026-02-13 22:48:11 +0800

    k

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,11 +4,6 @@ import { baseURL, routes as routesConfig } from "@/resources";
 export const dynamic = "force-static";
 
 export default async function sitemap() {
-  const blogs = getPosts(["src", "app", "blog", "posts"]).map((post) => ({
-    url: `${baseURL}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
-
   const works = getPosts(["src", "app", "work", "projects"]).map((post) => ({
     url: `${baseURL}/work/${post.slug}`,
     lastModified: post.metadata.publishedAt,
@@ -23,5 +18,5 @@ export default async function sitemap() {
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs, ...works];
+  return [...routes, ...works];
 }
--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -119,7 +119,15 @@ export default async function Project({
         </Row>
       </Row>
       {post.metadata.images.length > 0 && (
-        <Media priority aspectRatio="16 / 9" radius="m" alt="image" src={post.metadata.images[0]} />
+        <Row shadow="m" radius="m">
+          <Media
+            priority
+            aspectRatio="16 / 9"
+            radius="m"
+            alt="image"
+            src={post.metadata.images[0]}
+          />
+        </Row>
       )}
       <Column style={{ margin: "auto" }} as="article" maxWidth="xs">
         <CustomMDX source={post.content} />
